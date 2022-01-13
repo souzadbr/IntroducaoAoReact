cfbcfea1b1c7b6cf1214492b94ad07f739475e02
Debora Souza <deboral.p@hotmail.com>
2022-01-13 10:42:40 -0300

    Criar componente 'ComParamentro'

---
+++ src/components/ComParametro.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () =>
+
+<>
+    <h3>titulo</h3>
+    <p>subtitulo</p>
+</>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './index.css'
 import React from "react" // não é isada aqui diretamente , mas é necessaria para aplicação rodar. 
 import ReactDom from "react-dom" // reactDom é usado apensas dentro desse arquivo, todos os outros importamos o react.
-import Primeiro from './components/Primeiro'
 
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
 
 ReactDom.render(
-  <Primeiro/>,
+  <div>
+ <Primeiro/>
+ <ComParametro/>
+  </div>
+ ,
   document.getElementById('root')
 )
