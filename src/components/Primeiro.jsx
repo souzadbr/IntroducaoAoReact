4c64610211c16003869b0094be9c7d6cf8cdec89
Debora Souza <deboral.p@hotmail.com>
2022-01-13 10:13:15 -0300

    Refatorar primeiro componente usando div,h1 e h2

--- src/components/Primeiro.jsx
+++ src/components/Primeiro.jsx
@@ -1,7 +1,14 @@
+import react from "react"
 
 
 function Primeiro(){
-    return "Primeiro"
+    return (
+    <div>
+        <h1>Primeiro</h1>
+        <h2>Exemplo de um componente React</h2>
+    </div>
+    )
+        
 }
 
 export default Primeiro
