7cfe54b4ac71164cdafba485e1e5e2559be0c680
Debora Souza <deboral.p@hotmail.com>
2022-01-13 10:25:04 -0300

    Refatorar componente usando de Aroow Function

--- src/components/Primeiro.jsx
+++ src/components/Primeiro.jsx
@@ -1,14 +1,16 @@
-import react from "react"
+import React from "react"
 
 
-function Primeiro(){
-    return (
-    <div>
-        <h1>Primeiro</h1>
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () =>
+   
+    <>
+        <h1>Primeiro Componente (Arow)</h1>
         <h2>Exemplo de um componente React</h2>
-    </div>
-    )
-        
-}
+    </>
 
-export default Primeiro
+
+
+/* o h1 e h2 devem existir dentro de uma div, caso essa div sej´pa retirada do código ele quebra.
+ou usar tag vazia <> </> ou um <React.Fragment> </react.Fragment> */
